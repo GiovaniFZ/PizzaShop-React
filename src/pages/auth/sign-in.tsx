cce29e6a79faa4d788d33bac7daa9cb4cc56eeaa
GiovaniFZ <giovanifinazzi@gmail.com>
2024-10-24 13:06:55 -0300

    refactor: try/catch on handleSignIn

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -21,14 +21,18 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    toast.success('Enviamos um link de autenticação para seu email', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
+    try {
+      console.log(data)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu email', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas')
+    }
   }
 
   return (
